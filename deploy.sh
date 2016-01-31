9dae58404d12416e57fd8ab717ded4c62dd813bd
kelley <krobinson08@gmail.com>
2016-01-30 17:58:05 -0800

    Update deploy script to be more efficient

--- deploy.sh
+++ deploy.sh
@@ -3,24 +3,21 @@
 # deploy.sh
 
 rm -rf _site/
-export JEKYLL_ENV="production"
-jekyll build
+JEKYLL_ENV="production" jekyll build
 
 # deploy main site
-s3cmd put -r _site/ s3://krobinson.me
-s3cmd del s3://krobinson.me/blog.html
-s3cmd del -r s3://krobinson.me/posts/
+s3cmd put -r --exclude='blog.html' --exclude='posts/*' _site/ s3://krobinson.me
 
 # deploy blog
 s3cmd put _site/blog.html s3://blog.krobinson.me
 s3cmd put _site/error.html s3://blog.krobinson.me
-s3cmd put -r _site/posts/ s3://blog.krobinson.me/posts/
 
 # strip extension so we can serve clean urls
-for POST in `ls _site/posts/`; do
+for POST in $(ls -1 _site/posts/); do
     EXT=".html"
     STRIPPED=${POST%$EXT}
-    s3cmd mv s3://blog.krobinson.me/posts/$POST s3://blog.krobinson.me/posts/$STRIPPED
+    mv "_site/posts/$POST" "_site/posts/$STRIPPED"
 done
+s3cmd put -r -m text/html _site/posts/ s3://blog.krobinson.me/posts/
 
 export JEKYLL_ENV="development"
