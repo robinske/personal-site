00c80311ce8bacb2d38ecdc681a7a9e449f36dff
kelley <krobinson08@gmail.com>
2017-10-28 10:38:27 -0700

    More deploy script clean up

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,20 @@ rm -rf _site/
 JEKYLL_ENV="production" jekyll build
 
 # deploy main site
-s3cmd put -r --exclude='blog/*' --exclude='posts/*' --exclude='talks/*' _site/ s3://krobinson.me
+echo "Deploying main site..."
+s3cmd put -r --exclude='blog/*' --exclude='posts/*' --exclude='talks/*' ./_site/ s3://krobinson.me
 
 # deploy blog
-s3cmd sync -r _site/blog s3://blog.krobinson.me
-s3cmd sync -r _site/error s3://blog.krobinson.me
-s3cmd sync -r -m text/html _site/posts/ s3://blog.krobinson.me/posts/
+echo "Deploying blog..."
+s3cmd sync -r ./_site/blog/ s3://blog.krobinson.me
+s3cmd sync -r ./_site/error/index.html s3://blog.krobinson.me/error.html
+echo "Deploying blog posts..."
+s3cmd sync -r ./_site/posts/ s3://blog.krobinson.me/posts/
 
 # deploy talks
-s3cmd sync -r _site/talks s3://talks.krobinson.me
-s3cmd sync -r _site/error s3://blog.krobinson.me
+echo "Deploying talks..."
+s3cmd sync -r ./_site/talks/ s3://talks.krobinson.me
+s3cmd sync -r ./_site/error/index.html s3://talks.krobinson.me/error.html
 
 
 export JEKYLL_ENV="development"
