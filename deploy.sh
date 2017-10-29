b216e514d5b4e39795ea8b2bb3469044adea1f21
kelley <krobinson08@gmail.com>
2017-10-29 16:51:57 -0700

    ignore error sync

--- deploy.sh
+++ deploy.sh
@@ -10,14 +10,17 @@ s3cmd sync -r --exclude='blog/*' --exclude='posts/*' --exclude='talks/*' ./_site
 # deploy blog
 echo "Deploying blog..."
 s3cmd sync -r ./_site/blog/ s3://blog.krobinson.me
-s3cmd sync -r -m text/html ./_site/error/index.html s3://blog.krobinson.me/error.html
 echo "Deploying blog posts..."
 s3cmd sync -r -m text/html ./_site/posts/ s3://blog.krobinson.me/posts/
 
 # deploy talks
 echo "Deploying talks..."
 s3cmd sync -r ./_site/talks/ s3://talks.krobinson.me
-s3cmd sync -r -m text/html ./_site/error/index.html s3://talks.krobinson.me/error.html
+
+
+# This just does a copy, ignore it unless we need it for meow.
+# s3cmd sync -m text/html ./_site/error/index.html s3://blog.krobinson.me/error.html
+# s3cmd sync -r -m text/html ./_site/error/index.html s3://talks.krobinson.me/error.html
 
 
 export JEKYLL_ENV="development"
