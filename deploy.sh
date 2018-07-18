94bc7cbed617012e284fae11a2c7a6401ae659d8
Kelley Robinson <krobinson@twilio.com>
2018-07-17 17:39:27 -0700

    Add Hello Finatra blog post, update deploy script for Travis

--- deploy.sh
+++ deploy.sh
@@ -5,22 +5,16 @@ JEKYLL_ENV="production" jekyll build
 
 # deploy main site
 echo "Deploying main site..."
-s3cmd sync -r --exclude='blog/*' --exclude='posts/*' --exclude='talks/*' ./_site/ s3://krobinson.me
+s3cmd sync -r --access_key="$AWS_ACCESS_KEY" --secret_key="$AWS_SECRET_KEY" --exclude='blog/*' --exclude='posts/*' --exclude='talks/*' ./_site/ s3://krobinson.me
 
 # deploy blog
 echo "Deploying blog..."
-s3cmd sync -r ./_site/blog/ s3://blog.krobinson.me
+s3cmd sync -r --access_key="$AWS_ACCESS_KEY" --secret_key="$AWS_SECRET_KEY" ./_site/blog/ s3://blog.krobinson.me
 echo "Deploying blog posts..."
-s3cmd sync -r -m text/html ./_site/posts/ s3://blog.krobinson.me/posts/
+s3cmd sync -r --access_key="$AWS_ACCESS_KEY" --secret_key="$AWS_SECRET_KEY" -m text/html ./_site/posts/ s3://blog.krobinson.me/posts/
 
 # deploy talks
 echo "Deploying talks..."
-s3cmd sync -r ./_site/talks/ s3://talks.krobinson.me
-
-
-# This just does a copy, ignore it unless we need it for meow.
-# s3cmd sync -m text/html ./_site/error/index.html s3://blog.krobinson.me/error.html
-# s3cmd sync -r -m text/html ./_site/error/index.html s3://talks.krobinson.me/error.html
-
+s3cmd sync -r --access_key="$AWS_ACCESS_KEY" --secret_key="$AWS_SECRET_KEY" ./_site/talks/ s3://talks.krobinson.me
 
 export JEKYLL_ENV="development"
