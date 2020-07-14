12c1ecf1a0e5a22d2b39550b97dd3dc2b6233fb0
kelley <3673341+robinske@users.noreply.github.com>
2020-07-13 22:57:57 -0400

    Prefix all blog post paths with /blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/blog`,
+        path: `${__dirname}/content`,
         name: `blog`,
       },
     },
